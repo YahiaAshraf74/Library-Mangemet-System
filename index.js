ce411a23935dd5392a57da900d7dce65b9118cd8
yahiaaglan <yaglan@sahlpay.app>
2024-03-11 00:19:30 +0200

    adding rate limiter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/sequelize');
-require('./config/associations')(sequelize); // Make sure to call the function to define associations
+require('./config/associations')(sequelize);
+const rateLimiter = require("./middleware/rateLimiter");
 
 const bookRoutes = require('./routes/book');
 const borrowerRoutes = require('./routes/borrower');
@@ -10,6 +11,7 @@ const borrowingRoutes = require('./routes/borrowing');
 
 const app = express();
 app.use(express.json());
+app.use(rateLimiter);
 app.use('/api', bookRoutes);
 app.use('/api', borrowerRoutes);
 app.use('/api', borrowingRoutes);
---
+++ middleware/rateLimiter.js
@@ -0,0 +1,9 @@
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 10, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, try again later",
+});
+
+module.exports = limiter;
